6571e488457f303e46c65d740e5011d3e0abb7a5
isnipe <thalles.isnipe@gmail.com>
2021-09-11 15:28:20 -0300

    Making the benchmark test work

--- v2/jsleak/main_test.go
+++ v2/jsleak/main_test.go
@@ -8,22 +8,22 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-func BenchmarkReq(b *testing.B) {
+func BenchmarkReq_Old(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		reqhttp_old("http://testphp.vulnweb.com/", 5)
 	}
 }
 
-func BenchmarkReq(b *testing.B) {
+func BenchmarkReq_FastHttp(b *testing.B) {
 	c := &fasthttp.Client{
 		Name: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
 		TLSConfig: &tls.Config{
 			InsecureSkipVerify: true,
 		},
-		MaxConnWaitTimeout: time.Duration(*timeout) * time.Second,
+		MaxConnWaitTimeout: time.Duration(5) * time.Second,
 	}
 
 	for i := 0; i < b.N; i++ {
-		reqhttp("http://testphp.vulnweb.com/", 5)
+		reqhttp("http://testphp.vulnweb.com/", c)
 	}
 }
